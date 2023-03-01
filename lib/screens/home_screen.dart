115b82834863f4b338a1d264846ad430b507a7b4
johyonghoon <johyonghoon01@gmail.com>
2023-03-01 16:40:24 +0900

    image picker test

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,13 +1,70 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:http/http.dart' as http;
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  late File _image;
+
+  Future getImage() async {
+    final pickedFile =
+        await ImagePicker().pickImage(source: ImageSource.gallery);
+
+    setState(() {
+      _image = File(pickedFile!.path);
+    });
+  }
+
+  Future uploadImage() async {
+    final uri = Uri.parse('https://example.com/upload');
+    final request = http.MultipartRequest('POST', uri);
+    final file = await http.MultipartFile.fromPath('image', _image.path);
+
+    request.files.add(file);
+
+    final response = await request.send();
+
+    if (response.statusCode == 200) {
+      print("Image Upload");
+    } else {
+      print("Image Upload failed");
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text("Upload Photo"),
+      ),
       body: Center(
-        child: Text("Home Screen"),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              height: 20.0,
+            ),
+            ElevatedButton(
+              onPressed: getImage,
+              child: Text("Selected Image"),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            ElevatedButton(
+              onPressed: uploadImage,
+              child: Text("Upload Image"),
+            ),
+          ],
+        ),
       ),
     );
   }
